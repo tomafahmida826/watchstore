753662c529f193e0c9efa9b83f2be1b20a51ebdc
Fahmida Hossain <tomafahmida826@gmail.com>
2025-02-22 19:16:14 +0600

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,79 @@
+<footer class="page-footer">
+
+	<div class="footer-nav">
+		<div class="container clearfix">
+
+			<div class="footer-nav__col footer-nav__col--info">
+				<div class="footer-nav__heading">Information</div>
+				<ul class="footer-nav__list">
+					<li class="footer-nav__item">
+						<a href="#" class="footer-nav__link">The brand</a>
+					</li>
+				</ul>
+			</div>
+
+			<div class="footer-nav__col footer-nav__col--whybuy">
+				<div class="footer-nav__heading">Why buy from us</div>
+				<ul class="footer-nav__list">
+					<li class="footer-nav__item">
+						<a href="#" class="footer-nav__link">Shipping &amp; returns</a>
+					</li>
+					<li class="footer-nav__item">
+						<a href="#" class="footer-nav__link">Secure shipping</a>
+					</li>
+				</ul>
+			</div>
+
+			<div class="footer-nav__col footer-nav__col--account">
+				<div class="footer-nav__heading">Your account</div>
+				<ul class="footer-nav__list">
+					<li class="footer-nav__item">
+						<a href="#" class="footer-nav__link">Sign in</a>
+					</li>
+					<li class="footer-nav__item">
+						<a href="#" class="footer-nav__link">Register</a>
+					</li>
+					<li class="footer-nav__item">
+						<a href="#" class="footer-nav__link">View cart</a>
+					</li>
+				</ul>
+			</div>
+
+
+			<div class="footer-nav__col footer-nav__col--contacts">
+				<div class="footer-nav__heading">Contact details</div>
+				<address class="address">
+				Head Office: WatchStore <br>
+			</address>
+				<div class="phone">
+					Telephone:
+					<a class="phone__number" href="tel:0123456789">0123-456-789</a>
+				</div>
+				<div class="email">
+					Email:
+					<a href="mailto:support@yourwebsite.com" class="email__addr">contact@watchstore.com</a>
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+	
+
+	<div class="page-footer__subline">
+		<div class="container clearfix">
+
+			<div class="copyright">
+				&copy; 2025 WatchStore&trade;
+			</div>
+
+			<div class="developer">
+				Dev by Fahmida Hossain
+			</div>
+
+			<div class="designby">
+				Design by Fahmida Hossain
+			</div>
+		</div>
+	</div>
+</footer>
